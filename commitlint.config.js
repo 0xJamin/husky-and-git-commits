6980ad0c4d57641be2a8eea7b006eff3a77e615d
Jamin <okpusjamin@gmail.com>
2023-12-23 15:32:07 +0000

    [task-13] chore: update build process

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', ['project-task']],
-    'scope-case': [2, 'always', 'kebab-case'],
-    'scope-empty': [2, 'always'],
-  },
+  extends: ["@commitlint/config-conventional"],
 };
